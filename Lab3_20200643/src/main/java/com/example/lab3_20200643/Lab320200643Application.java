cfcdb3a52a1f9a795a6f9f6682dea549f901c975
jorgeluis <jorgel.dominguez03@gmail.com>
2023-04-11 20:38:17 -0500

    proyecto

---
+++ Lab3_20200643/src/main/java/com/example/lab3_20200643/Lab320200643Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20200643;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320200643Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab320200643Application.class, args);
+    }
+
+}
---
+++ Lab3_20200643/src/test/java/com/example/lab3_20200643/Lab320200643ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20200643;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320200643ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
